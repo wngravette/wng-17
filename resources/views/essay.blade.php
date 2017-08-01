44edcd536aed88f40f01a61b0fdce9bd9f4959fc
William‌‌ Naughton-Gravette <william@willng.me>
2017-08-01 16:42:21 +1000

    Create essay.blade.php

---
+++ resources/views/essay.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>William Naughton-Gravette</title>
+        <link href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <div class="pure-g">
+                <div class="pure-u-4-24">
+
+                </div>
+                <div class="pure-u-16-24">
+                    <div class="l-box">
+                        <h1>William Naughton-Gravette &#8212; <span>reading Politics &amp; International Relations</span></h1>
+                    </div>
+                </div>
+                <div class="pure-u-4-24">
+
+                </div>
+            </div>
+        </header>
+        <div class="pure-g">
+            <div class="pure-u-4-24">
+
+            </div>
+            <div class="pure-u-10-24">
+                <div class="pure-u-1">
+                    <div class="l-box">
+                        <p>
+                           
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="pure-u-1-24">
+
+            </div>
+            <div class="pure-u-5-24 sidebar">
+                <div class="pure-u-1">
+                    <div class="l-box">
+                        <p>
+                            <span>Longer Writings</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 sidebar-item">
+                    <div class="l-box">
+                        <p>
+                            <i>Understanding the Copenhagen School of Security Studies as a Lens to Approach the Problem of Global Climate Change <span>&#8212; 2017 &middot; 3261 words</span></i>
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 sidebar-item">
+                    <div class="l-box">
+                        <p>
+                            <i>A Discussion of the Relationship Between Resource Dependence and the Existence and Endurance of Authoritarianism in Africa <span>&#8212; 2017 &middot; 3324 words</span></i>
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 sidebar-item">
+                    <div class="l-box">
+                        <p>
+                            <i>Approaching Realist Arguments for the Iraq War (with a brief history of Realism) <span>&#8212; 2016 &middot; 2804 words</span></i>
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 sidebar-item">
+                    <div class="l-box">
+                        <p>
+                            <i>The Media Cycle & Corporate Pressures of the 21st Century, and its Implications for Investigative Journalism <span>&#8212; 2016 &middot; 2301 words</span></i>
+                        </p>
+                    </div>
+                </div>
+                <div class="pure-u-1 sidebar-item">
+                    <div class="l-box">
+                        <p>
+                            <i>Evaluating Australia's Increasingly Unstable Electorate <span>&#8212; 2016 &middot; 2197 words</span></i>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="pure-u-5-24">
+
+            </div>
+        </div>
+    </body>
+</html>
